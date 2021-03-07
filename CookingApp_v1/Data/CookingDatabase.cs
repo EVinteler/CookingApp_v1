fef6b5caded814bc975b86c53b93534f58e82721
EVinteler <ericavinteler@yahoo.com>
2021-03-07 17:46:25 +0200

    created database

---
+++ CookingApp_v1/Data/CookingDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+using System.Threading.Tasks;
+using CookingApp_v1.Models;
+
+namespace CookingApp_v1.Data
+{
+    class CookingDatabase
+    {
+    }
+}
---
+++ CookingApp_v1/Models/Utilizatori.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+
+namespace CookingApp_v1.Models
+{
+    public class Utilizatori
+    {
+        [PrimaryKey, AutoIncrement]
+        public int U_id { get; set; }
+        public string U_nume { get; set; }
+        public string U_email { get; set; }
+        public string U_parola { get; set; }
+
+        // daca nu merge cu 1:M, schimba in string si converteste la cautare
+        public string U_frigider { get; set; }
+        public string U_filtre { get; set; }
+    }
+}
